8e99fd3ff914ebba15107470537532bc433a4c82
eXsio <sdymitrow@gmail.com>
2016-05-11 15:34:10 +0200

    always publish event in a new transaction

--- domain/src/main/java/com/ddd/poc/domain/core/service/EventBus.java
+++ domain/src/main/java/com/ddd/poc/domain/core/service/EventBus.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -25,7 +26,7 @@ public class EventBus {
     }
 
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void publishEvent(DomainEvent event) {
         try {
             eventDomainDao.create(event).save();
